51af984785defc03f9ae310f62c253b7e3c1826a
weslyramalho <weslyramalho@yahoo.com.br>
2023-01-30 15:39:05 -0300

    buscaCep atualizado

--- http/main.go
+++ http/main.go
@@ -1,11 +1,71 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCep struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
-	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/", BuscaCEPHandler)
 	http.ListenAndServe(":8080", nil)
 }
-func BuscaCEP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, word!!"))
+func BuscaCEPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cep, error := BuscaCep(cepParam)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//salvando
+	/*
+		result, err := json.Marshal(cep)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	*/
+	json.NewEncoder(w).Encode(cep)
+}
+
+func BuscaCep(cep string) (*ViaCep, error) {
+	resp, error := http.Get("https://viacep.com.br/ws/" + cep + "/json")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := ioutil.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	var c ViaCep
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+	return &c, nil
 }
