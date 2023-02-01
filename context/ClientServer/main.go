03805a969eaaface57ad4e04dcf6494567f90277
weslyramalho <weslyramalho@yahoo.com.br>
2023-01-31 21:38:43 -0300

    server context

---
+++ context/ClientServer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log.Println("Request iniciada")
+	defer log.Println("Request finalizada")
+	select {
+	case <-time.After(5 * time.Second):
+		//IMPRIME NO COMAND LINE STDOUT
+		log.Println("Request processada com sucesso")
+		//imprime no browser
+		w.Write([]byte("Request processada com sucesso"))
+	case <-ctx.Done():
+		log.Println("Request cancelada pelo cliente")
+		//http.Error(w, "Request cancelada pelo cliente", http.StatusRequestTimeout)
+	}
+}
