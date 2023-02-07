22e1fbc645f9515b6443936ddc77ab916809d7ef
weslyramalho <weslyramalho@yahoo.com.br>
2023-02-07 20:12:25 -0300

    busca por id

--- BancodeDados/main.go
+++ BancodeDados/main.go
@@ -60,3 +60,17 @@ func uptadeProduct(db *sql.DB, product *Product) error {
 	}
 	return nil
 }
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
